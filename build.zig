d1e1038cf513302c9c6f11457fb1c23053d4a7f4
clickingbuttons <clickingbuttons@pm.me>
2023-08-04 12:17:22 -0400

    upgrade to zig 0.11.0

--- build.zig
+++ build.zig
@@ -1,21 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // Expose to zig dependents
+    _ = b.addModule("lz4", .{ .source_file = .{ .path = "src/lib.zig" } });
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Expose to zig dependents
-    _ = b.addModule("lz4", .{
-        .source_file = .{ .path = "src/lib.zig" },
-        .dependencies = &.{},
-    });
-
-    const unit_tests = b.addTest(.{
+    const test_step = b.step("test", "Run unit tests");
+    const run_tests = b.addRunArtifact(b.addTest(.{
         .root_source_file = .{ .path = "src/lib.zig" },
         .target = target,
         .optimize = optimize,
-    });
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    }));
+    test_step.dependOn(&run_tests.step);
 }
